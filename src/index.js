315322afd8e0e79158457a1d5c42ecdfcc76d753
Роман Маскаев <subulka@mail.ru>
2023-08-26 11:38:46 +0300

    1V

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const sliderContainerElement = document.querySelector('.slider__elements');
 if(sliderContainerElement) {
 const CardforSlider = new Section({items:dataPeterburgCards,
      renderer: (item) =>{
-     const card = new Card(item,'#card')
+     const card = new Card({item:item,handleLikeClick:(evt)=>{
+          card.likeCard(evt)
+     }},'#card')
      const cardElement = card.generate()
      CardforSlider.addItem(cardElement)
 }}, '.slider__elements')
