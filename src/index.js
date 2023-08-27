2e14e503f63a8055f9b61db6a21f37f74f927902
Izvolsky <9961200@mail.ru>
2023-08-27 12:12:30 +0300

    добавил условие проверки наличия фильтр-тегов на странице

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./pages/index.scss";
 import FilterTag from "./scripts/filter-tags";
-const filterTagEvent = new FilterTag(".container-tags", ".filter-tag");
 
-filterTagEvent.setEventListeners();
+const containerTags = document.querySelector(".container-tags");
+if (containerTags) {
+  const filterTagEvent = new FilterTag(".container-tags", ".filter-tag");
+  filterTagEvent.setEventListeners();
+}
