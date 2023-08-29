ef5582a0b2e22aede3d10f138be78d4a2c5454db
Ekaterina <turovaes@gmail.com>
2023-08-29 22:09:25 +0300

    Добавлена логика для слайдера в photo-galary компоненте

--- src/index.js
+++ src/index.js
@@ -5,23 +5,28 @@ import Card from './scripts/Card';
 import { dataPeterburgCards } from './scripts/dataPeterburgCards';
 import popupCard from './scripts/PopupCard';
 import { LocationPopover } from './scripts/Popover';
+import { PhotoGallery } from './scripts/PhotoGallery';
 
 /* карточки для слайдера */
 const sliderContainerElement = document.querySelector('.slider__elements');
-const cardPopup= new popupCard('.popup');
-if(sliderContainerElement) {
-const CardforSlider = new Section({items:dataPeterburgCards,
-     renderer: (item) =>{
-     const card = new Card({item:item, handleLikeClick:(evt)=>{
-          card.likeCard(evt)
-     }, handleCardClick: ()=>{
-          cardPopup.open(item)
-          cardPopup.setEventListeners()
-     }},'#card')
-     const cardElement = card.generate()
-     CardforSlider.addItem(cardElement)
-}}, '.slider__elements')
-CardforSlider.renderItems()
+const cardPopup = new popupCard('.popup');
+if (sliderContainerElement) {
+     const CardforSlider = new Section({
+          items: dataPeterburgCards,
+          renderer: (item) => {
+               const card = new Card({
+                    item: item, handleLikeClick: (evt) => {
+                         card.likeCard(evt)
+                    }, handleCardClick: () => {
+                         cardPopup.open(item)
+                         cardPopup.setEventListeners()
+                    }
+               }, '#card')
+               const cardElement = card.generate()
+               CardforSlider.addItem(cardElement)
+          }
+     }, '.slider__elements')
+     CardforSlider.renderItems()
 }
 
 // Выбор локации
@@ -31,6 +36,24 @@ locationPopover.setListItems(['Москва', 'Санкт-Петербург', '
 /* фильтр-тэги */
 const containerTags = document.querySelector(".container-tags");
 if (containerTags) {
-  const filterTagEvent = new FilterTag(".container-tags", ".filter-tag");
-  filterTagEvent.setEventListeners();
+     const filterTagEvent = new FilterTag(".container-tags", ".filter-tag");
+     filterTagEvent.setEventListeners();
 }
+
+// Слайдер для фото-галереи
+
+const photoGallery = new PhotoGallery({
+     rootSelector: '.gallery',
+     imageClass: 'gallery__image',
+     sliderSelector: '.gallery__slider',
+     sliderItemClass: 'gallery__slider-item',
+     activeImageClass: 'gallery__image_mobile_visible',
+     activeSliderItemClass: 'gallery__slider-item_active'
+});
+photoGallery.setImages([
+     { src: '../images/gallery/image-park.svg', alt: 'Парк' },
+     { src: '../images/gallery/image-girl.svg', alt: 'Девушка' },
+     { src: '../images/gallery/image-coctails.svg', alt: 'Коктели' },
+     { src: '../images/gallery/image-exhibition.svg', alt: 'Выставка' },
+     { src: '../images/gallery/image-party.svg', alt: 'Вечеринка' }
+]);
