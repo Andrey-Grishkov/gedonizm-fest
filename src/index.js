dfa4a83db0e4f862db84f7fd9b38c91346030d0e
andrey.tyazhelomov <andrey.tyazhelomov@1win.pro>
2023-08-30 09:14:02 +0300

    feature/festival | fixes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './pages/index.scss';
+import { buttonUp } from './scripts/constants';
+import ButtonUpManager from './components/ButtonUpManager';
+
+new ButtonUpManager(buttonUp).addEventListener();
