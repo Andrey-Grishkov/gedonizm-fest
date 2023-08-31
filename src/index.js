b7025e5e974334fe4511d56fcb12be636102e350
Роман Маскаев <subulka@mail.ru>
2023-08-31 20:22:32 +0300

    1V

--- src/index.js
+++ src/index.js
@@ -6,19 +6,16 @@ import {
   containerTagsTypeDays,
   containerTagsTypeEvt,
 } from "./scripts/constants";
-import { buttonUp, configPhotoGallery, initialPhotoGalleryImages, buttonSupport } from './scripts/constants';
+import { buttonUp, configPhotoGallery, initialPhotoGalleryImages, buttonSupport ,buttonCardLikes} from './scripts/constants';
 import ButtonUpManager from './components/ButtonUpManager';
 import FilterTag from "./scripts/filter-tags";
 import Section from './scripts/section';
 import Card from './scripts/Card';
 import { dataPeterburgCards } from './scripts/dataPeterburgCards';
 import popupCard from './scripts/PopupCard';
-import { LocationPopover } from './scripts/Popover
+import { LocationPopover } from './scripts/Popover'
 import popupLikesCard from './scripts/popupLikesCards';
-import { buttonCardLikes } from './scripts/constants';
 /* карточки для слайдера */
-const sliderContainerElement = document.querySelector('.slider__elements');
-const cardPopup = new popupCard('#Card-popup');
 const cardlikePopup = new popupLikesCard('#Likescard-popup')
 import { PhotoGallery } from './scripts/PhotoGallery';
 import Popup from './scripts/popup.js';
@@ -54,13 +51,11 @@ if (sliderContainerElement) {
      }, '.slider__elements')
      CardforSlider.renderItems()
 }
-if(buttonCardLikes){
 buttonCardLikes.addEventListener('click',function(){
      cardlikePopup.open()
      cardlikePopup.setEventListeners()
 })
 
-}
 // Выбор локации
 const locationPopover = new LocationPopover('#location-popover', '.header__location');
 locationPopover.setListItems(['Москва', 'Санкт-Петербург', 'Сочи', 'Калуга', 'Екатеринбург'], 'Санкт-Петербург');
@@ -78,11 +73,12 @@ if (galleryContainerElement) {
 }
 
 // попап с пожертвованием
+if(buttonSupport){
 buttonSupport.addEventListener('click', function () {
      supportPopup.open()
      supportPopup.setEventListeners()
 })
-
+}
 /* фильтр-тэги */
 const containerTags = document.querySelector(".container-tags");
 
