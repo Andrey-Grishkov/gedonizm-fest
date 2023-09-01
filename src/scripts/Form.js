483e3da3443c33356a8878422d89ff56e48f9e8a
Dmitrii Valentinov <d.valentinov@gmail.com>
2023-09-01 21:45:57 +0300

    загрузка и удаление фото

--- src/scripts/Form.js
+++ src/scripts/Form.js
@@ -13,6 +13,16 @@ export class Form {
       this._checkboxesTypeEvent = document.querySelectorAll('.form__step-checkbox_type-event');
       this._blocksCategory = document.querySelectorAll('[data-category-id]');
       this._blocksOnline = document.querySelectorAll('[data-type-event]');
+
+      this._partyType = document.querySelector('.form__radio_party-type');
+      this._stepFirstData = document.querySelector('.form__step-radiobutton_checked').querySelectorAll('.form__step-caption');
+
+      this._dropZone = document.querySelector('.form__foto');
+      this._fotoIcon = document.querySelector('.form__foto-icon');
+      this._fotoImage = document.querySelector('.form__foto-image');
+      this._fotoClose = document.querySelector('.form__foto-close');
+      this._fotoLabel = document.querySelector('.form__foto-label');
+      this._fotoInput = document.querySelector('.form__input-file');
   }
 
   setEventListener() {
@@ -27,7 +37,7 @@ export class Form {
       button.addEventListener('click', this._handleBack)
     });
 
-    this._form.addEventListener('submit', (event) => this._handleSubmitForm(event));
+    this._form.addEventListener('submit', (event) => this._handleSubmitForm.call(this, event));
 
     this._checkboxesCategory.forEach((checkbox) => {
       checkbox.addEventListener('click', (event) => {this._handleCategory(event)});
@@ -39,6 +49,15 @@ export class Form {
         this._setVisibleInputs();
       });
     });
+
+    this._dropZone.addEventListener('dragover', (event) => this._handleDragover(event));
+
+    this._dropZone.addEventListener('drop', (event) => this._handleDrop.call(this, event));
+
+    this._dropZone.addEventListener('click', (event) => this._handleImageClick.call(this, event));
+
+    this._fotoClose.addEventListener('click', (event) => this._handleCloseFoto.call(this, event));
+
   }
 
   _setVisibleInputs() {
@@ -85,14 +104,13 @@ export class Form {
     event.preventDefault();
 
     let dataTotal = {};
-    dataTotal["мероприятие"] = this._getEventName();
+    dataTotal["мероприятие"] = this._getEventName.call(this);
     dataTotal["тип мероприятия"] = this._typeEvent;
-    const partyType = document.querySelector('.form__radio_party-type');
-    if (partyType.dataset.categoryId === this._nameCategory) {
-      dataTotal['тип вечеринки'] = this._getTypeParty(partyType);
+
+    if (this._partyType.dataset.categoryId === this._nameCategory) {
+      dataTotal['тип вечеринки'] = this._getTypeParty(this._partyType);
     };
     dataTotal = this._getInputsData(dataTotal);
-
     console.log(dataTotal);
   }
 
@@ -103,14 +121,57 @@ export class Form {
     };
   }
 
+  _handleDragover(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+  }
+
+  _handleDrop(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    const fileList = event.dataTransfer.files;
+    let file = fileList[0];
+    this._loadImage(file);
+  }
+
+  _handleImageClick(event) {
+    this._fotoInput.click();
+    this._fotoInput.onchange = e => {
+      let file = e.target.files[0];
+      if (file) {
+        this._loadImage(file);
+      }
+   }
+  }
+
+  _loadImage(file) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this._fotoImage.src = reader.result;
+      this._fotoImage.classList.remove('hidden');
+      this._fotoLabel.classList.add('hidden');
+      this._fotoIcon.classList.add('hidden');
+      this._fotoClose.classList.remove('hidden');
+    };
+  }
+
   _handleTypeEvent(event) {
     this._typeEvent = event.target.value;
   }
 
+  _handleCloseFoto(event) {
+    this._fotoImage.src = "#";
+    this._fotoImage.classList.add('hidden');
+    this._fotoLabel.classList.remove('hidden');
+    this._fotoIcon.classList.remove('hidden');
+    this._fotoClose.classList.add('hidden');
+  }
+
   _getEventName() {
     let result='';
-    const stepFirstData = document.querySelector('.form__step-radiobutton_checked').querySelectorAll('.form__step-caption');
-    stepFirstData.forEach((data) => {
+    this._stepFirstData.forEach((data) => {
       if (!data.classList.contains('hidden')) {
         result = data.textContent;
         return result;
